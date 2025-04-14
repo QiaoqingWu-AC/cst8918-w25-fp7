0f67135abc8f9804955bc55ca02735697722ab74
QiaoqingWu-AC <wu000302@algonquinlive.com>
2025-04-14 12:21:20 -0400

    Add base terraform configs with remote backend setup

---
+++ infra/tf-backend/outputs.tf
@@ -0,0 +1,17 @@
+# Outputs
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.sa.name
+}
+
+output "storage_container_name" {
+  value = azurerm_storage_container.container.name
+}
+
+output "storage_account_access_key" {
+  value     = azurerm_storage_account.sa.primary_access_key
+  sensitive = true
+}
