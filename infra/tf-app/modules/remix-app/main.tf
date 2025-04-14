11fab8d62afd5b6cb1f03d58b779114235819814
Donna Ha <65718414+Donna-Smee@users.noreply.github.com>
2025-04-14 18:59:35 -0400

    feat: add terraform code for container registry, redis cache for test, prod
    - stated provider kubernetes

---
+++ infra/tf-app/modules/remix-app/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      configuration_aliases = [kubernetes.test, kubernetes.prod]
+    }
+  }
+}
+
+provider "kubernetes" {
+  alias = "test"
+}
+
+provider "kubernetes" {
+  alias = "prod"
+}
+
+
+# Container registry to store weather app docker image
+resource "azurerm_container_registry" "acrgroup7" {
+  name                = "remixarcgroup7cst8918"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+# Managed Redis DB to cache weather API data (in test and prod env)
+resource "azurerm_redis_cache" "redis" {
+  for_each            = toset(["test", "prod"])
+
+  name                = "remix-${each.key}-redis"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  capacity            = 0
+  family              = "C"
+  sku_name            = "Basic"
+  
+}
+
